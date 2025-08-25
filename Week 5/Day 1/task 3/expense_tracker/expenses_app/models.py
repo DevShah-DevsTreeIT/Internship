2d41932a75fb7f34f934c020b6a31c87a0f98fc9
DevShah-DevsTreeIT <dev.s@devstree.in>
2025-08-25 11:58:24 +0530

    w5d1 task3-Database-Driven Application

---
+++ Week 5/Day 1/task 3/expense_tracker/expenses_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+from django.db import models
+
+class Expense(models.Model):
+    title = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - ₹{self.amount}"
